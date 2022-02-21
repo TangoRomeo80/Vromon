a22fbe735d70130631d332641388af5e58578dbc
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-02-22 01:50:39 +0600

    updated inital structure

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container fluid className='bg-dark'>
+        <Row>
+          <Col className='d-flex justify-content-center text-light py-2'>
+            Copyright &copy; Unifly-Travels
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
