50b4f7204691085dd08f1d777b620f4048cfe643
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-07-15 22:23:55 +0600

    added new structuring

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const app = express() //create an instance of express
 
 //route for the root path
 app.get('/', (req, res) => {
-  res.status(200).send('API is running...')
+  res.status(200).json({ message: 'API is running...', app: 'Vromon' })
 })
+
+
 
 module.exports = app //export ap for use in server.js
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require('./app') //import the express app
 
+//listen to port 5000
 const PORT = 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
