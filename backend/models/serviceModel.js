7b7a7a26ec491bd6abeb5d0f5383c9c690ba894c
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-07-18 14:11:18 +0600

    added initial service schema

---
+++ backend/models/serviceModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose') //import mongoose from
+
+//create a services schema
+const serviceSchema = new mongoose.Schema(
+  {
+    destinationID: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    serviceName: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    businessId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    serviceMobileNumber: {
+      type: String,
+      required: true,
+    },
+    serviceType: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: false,
+    },
+    ratingQuantity: {
+      type: Number,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Service = mongoose.model('Service', serviceSchema) //create a model
+
+module.exports = Service //export the model
