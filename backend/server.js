1baade02d890c4b1cb73292657ca71e0715a7dab
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-07-18 13:53:28 +0600

    added database

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,19 @@
   This file contains all the server configurations and functionalities.
 */
 const dotenv = require('dotenv') //import dotenv from dotenv
+const mongoose = require('mongoose') //import mongoose ODM library
 const app = require('./app') //import the express app
 
 dotenv.config() //load the .env file
+
+//connect DB
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connnected successfully')
+  })
 
 //listen to port 5000
 const PORT = process.env.PORT
