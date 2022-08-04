78105913b86e9adea68914e3276df24d7100757b
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-04 18:20:55 +0600

    Update destinationRoutes.js

--- backend/routes/destinationRoutes.js
+++ backend/routes/destinationRoutes.js
@@ -1,18 +1,21 @@
 /* This file handles the routes related to the destinations */
 
-import express from "express"; //import express
+import express from 'express' //import express
 import {
   createDestination,
   updateDestination,
   deleteDestination,
   getAllDestinations,
   getDestination,
-} from "../controllers/destinationController.js"; //import destination controller
+} from '../controllers/destinationController.js' //import destination controller
 
 const router = express.Router() //create router instance
 
 router.route('/').get(getAllDestinations).post(createDestination)
-router.route('/most-popular').get(getAllDestinations)
-router.route('/:id').get(getDestination).patch(updateDestination).delete(deleteDestination)
+router
+  .route('/:id')
+  .get(getDestination)
+  .patch(updateDestination)
+  .delete(deleteDestination)
 
 export default router
