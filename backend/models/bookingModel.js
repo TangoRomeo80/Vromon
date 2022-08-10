b52b3cf52ebb821abcbb2e8277ecf1d53e8e4fb8
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-08-10 16:35:22 +0600

    Booking Model Added

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -5,12 +5,48 @@ import mongoose from 'mongoose'
 //Create a Booking Schema
 const bookingSchema = new mongoose.Schema(
     {
-        
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, 'User ID is Required']
+        },
+        service: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Service',
+            required: [true, 'Service Type is Required']
+        },
+        bookingDate: {
+            type: Date,
+            required: [true, 'Booking Date is Required']
+        },
+        availedDate: {
+            type: Date,
+            required: [true, 'Service Availing Date is Required']
+        },
+        paymentStatus: {
+            type: String,
+            required: [true, 'Payment Status is Required'],
+            trim: true,
+        },
+        paymentAmount: {
+            type: Number,
+            required: [true, 'Payment Amount is Required'],
+            trim: true,
+            maxlength: [10, 'Amount Number Cannot be More Than 10 Digits']
+        },
+        bookingStatus: {
+            type: String,
+            required: [true, 'Booking status is Required'],
+            trim: true,
+        }
+    },
+    {
+        timestamps: true
     }
 )
 
 
 //Create Booking Model
-const Booking = mongoose.model('Booking',)
+const Booking = mongoose.model('Booking',bookingSchema)
 
 export default Booking
