0f35f94343197c8d4ebd15e5ca8a1e5e5bb2934f
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-08-14 11:03:45 +0600

    Loader Created

---
+++ frontend/src/screens/UserInfoScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap'
+
+
+const UserInfoScreen = () => {
+  return (
+    <div>UserInfoScreen</div>
+  )
+}
+
+export default UserInfoScreen
