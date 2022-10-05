63636bcc3a33d9407aa6edde35e3b4a3c7ef0b15
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-05 21:16:25 +0600

    implement user information change by user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -122,6 +122,52 @@ export const updateUser = updateOne(User)
 // })
 
 /*
+  Request type: PATCH
+  Endpoint: /api/users/updateMe
+  Description: This endpoint updates info about currently logged in user
+*/
+
+export const updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    )
+  }
+
+  // 2) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  })
+})
+
+/*
+  Request type: DELETE
+  Endpoint: /api/users/deleteMe
+  Description: This endpoint deletes info about currently logged in user
+*/
+
+export const deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndDelete(req.user._id)
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  })
+})
+
+/*
   Request type: DELETE
   Endpoint: /api/users/:id
   Description: This endpoint deletes a specific user
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,8 @@ import {
   createUser,
   updateUser,
   deleteUser,
+  updateMe,
+  deleteMe,
 } from '../controllers/userController.js' //import User controller
 
 import {
@@ -37,6 +39,8 @@ router.route('/auth/:id').get(getAuthedUser) //route to get data from external s
 router.route('/forgotPassword').post(forgotPassword) //route to handle forgot password
 router.route('/resetPassword/:token').post(resetPassword) //route to handle reset password after forgetting password
 router.route('/updatePassword/:id').post(updatePassword) //route to handle update password
+router.route('/updateMe').patch(updateMe) //route to handle profile information update by user
+router.route('/deleteMe').delete(deleteMe) //route to handle profile deletion by user
 
 //base CRUD functionality for admin only
 router.route('/').get(getAllUsers).post(createUser)
