38b0a10d704294a6eb4fa7e9e18550fd40d58ae3
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-10 03:17:35 +0600

    fix store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import './bootstrap.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { store } from './app/store'
 import { Provider } from 'react-redux'
-import store from './app/store'
 
 ReactDOM.render(
   <React.StrictMode>
