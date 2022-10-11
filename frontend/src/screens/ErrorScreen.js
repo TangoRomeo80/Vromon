41bad68946a1db05d70d00cc02198517fec740bc
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-11 09:42:35 +0600

    fix css

--- frontend/src/screens/ErrorScreen.js
+++ frontend/src/screens/ErrorScreen.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const ErrorScreen = () => {
-  return (
-    <div>ErrorScreen</div>
-  )
+  return <div style={{ backgroundColor: 'red' }}>ErrorScreen</div>
 }
 
 export default ErrorScreen
