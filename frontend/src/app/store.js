4d85c952185e88bf6a9161b1b9cb81bf65c63a8d
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-15 20:43:11 +0600

    Update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import serviceReducer from '../features/service/serviceSlice'
 import authReducer from '../features/auth/authSlice'
 import businessReducer from '../features/business/businessSlice'
+import userReducer from '../features/user/userSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     service: serviceReducer,
     business: businessReducer,
+    user: userReducer,
   },
 })
