6d1b2b38073fd10fbb2919d503394cd076d70744
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-10-20 12:34:33 +0600

    Test Navbar Holiday Packages Initial Phase done

--- frontend/src/screens/TestNavbar.js
+++ frontend/src/screens/TestNavbar.js
@@ -311,10 +311,69 @@ const SearchDestinations = () => {
 };
 
 const SearchTours = () => {
+  const [searchTourCity, setSearchTourCity] = useState("");
+  const [travelDate, setTravelDate] = useState("");
+  const [travelerCount, setTravelerCount] = useState("");
+
   return (
-    <div>
-      <h1>Search Tours</h1>
-    </div>
+    <Card>
+      <Row>
+        <Card.Text as="h5">Search Holiday Packages</Card.Text>
+      </Row>
+
+      <Row className="my-5 mx-3">
+        <Col>
+          <Card.Text>Enter City</Card.Text>
+          <Form.Group className="mb-3" controlId="searchTourCity">
+            <Form.Control
+              type="text"
+              placeholder="Enter the City Name"
+              value={searchTourCity}
+              onChange={(e) => setSearchTourCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+
+        <Col>
+          <Card.Text>Travel Date</Card.Text>
+          <Form.Group className="mb-3" controlId="travelDate">
+            <InputGroup>
+              <div
+                onClick={(e) => {
+                  setTravelDate("");
+                }}
+                className="cancle-icon"
+                style={{
+                  position: "absolute",
+                  right: "5px",
+                  top: "5px",
+                  zIndex: "9999",
+                  width: "3vh",
+                }}
+              ></div>
+            </InputGroup>
+            <Form.Control
+              type="date"
+              placeholder={travelDate === "Select Check-In Date"}
+              value={travelDate}
+              onChange={(e) => setTravelDate(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+
+        <Col>
+          <Card.Text>Number of Travelers</Card.Text>
+          <Form.Group className="mb-3" controlId="travelerCount">
+            <Form.Control
+              type="text"
+              placeholder="Number of Travelers"
+              value={travelerCount}
+              onChange={(e) => setTravelerCount(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Card>
   );
 };
 
