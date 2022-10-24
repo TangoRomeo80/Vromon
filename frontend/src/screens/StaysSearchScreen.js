504016d1d33d66ab1382323ee0d5781fe1ffe034
diponkorcs <diponkorchandro678@gmail.com>
2022-10-24 15:20:47 +0600

    StaysSearch Updated

--- frontend/src/screens/StaysSearchScreen.js
+++ frontend/src/screens/StaysSearchScreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Container, Card, Form } from "react-bootstrap";
+import { Row, Col, Container, Card, Form, Button } from "react-bootstrap";
+import { MdLocationOn } from "react-icons/md";
 
 const StaysSearchScreen = () => {
-    const [maxPrice, setMaxPrice] = useState('')
-    const [minPrice, setMinPrice] = useState('')
-    const [searchHotel, setSearchHotel] = useState('')
+  const [maxPrice, setMaxPrice] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [searchHotel, setSearchHotel] = useState("");
   return (
     <Container>
       <Row className="mb-2 pt-3">
@@ -31,26 +32,26 @@ const StaysSearchScreen = () => {
             </Link>
           </Row>
 
-          <Row className='my-4'>
+          <Row className="my-4">
             <Card>
-              <Card.Header as='h5'>Price Range</Card.Header>
+              <Card.Header as="h5">Price Range</Card.Header>
             </Card>
 
             <Col sm={6} md={3} lg={6}>
-              <Form.Group className='mb-3' controlId=''>
-                <Form.Label className='small mb-1'>Minimum Price</Form.Label>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label className="small mb-1">Minimum Price</Form.Label>
                 <Form.Control
-                  type='text'
+                  type="text"
                   value={minPrice}
                   onChange={(e) => setMinPrice(e.target.value)}
                 ></Form.Control>
               </Form.Group>
             </Col>
             <Col sm={6} md={3} lg={6}>
-              <Form.Group className='mb-3' controlId=''>
-                <Form.Label className='small mb-1'>Maximum Price</Form.Label>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label className="small mb-1">Maximum Price</Form.Label>
                 <Form.Control
-                  type='text'
+                  type="text"
                   value={maxPrice}
                   onChange={(e) => setMaxPrice(e.target.value)}
                 ></Form.Control>
@@ -60,16 +61,16 @@ const StaysSearchScreen = () => {
           </Row>
 
           {/* Row For Hotel Search */}
-          <Row className='my-4'>
+          <Row className="my-4">
             <Card>
-              <Card.Header as='h5'>Search Hotels</Card.Header>
+              <Card.Header as="h5">Search Hotels</Card.Header>
             </Card>
 
-            <Col className='mt-3'>
-              <Form.Group className='mb-3' controlId=''>
+            <Col className="mt-3">
+              <Form.Group className="mb-3" controlId="">
                 <Form.Control
-                  type='text'
-                  placeholder='Enter Hotel Name'
+                  type="text"
+                  placeholder="Enter Hotel Name"
                   value={searchHotel}
                   onChange={(e) => setSearchHotel(e.target.value)}
                 ></Form.Control>
@@ -78,17 +79,56 @@ const StaysSearchScreen = () => {
           </Row>
         </Col>
 
-
         {/* Right Colomn/Package Images Card */}
         <Col xs={12} md={9} xl={9}>
-        <Row className="my-4">
-            <Card.Header as='h5' className="mx-4">
+          <Row className="my-4">
+            <Card.Header as="h5" className="mx-4">
               20 Available Hotels
             </Card.Header>
-            <Card.Text className='mx-3 mt-2'>
+            <Card.Text className="mx-3 mt-2">
               *Price is per night per room & includes VAT & Taxes
             </Card.Text>
           </Row>
+
+          <Card className="mx-2 mb-3">
+            <Row>
+              <Col sm={4} md={3} lg={3}>
+                <Card.Img
+                  src="/Destinations/Test.jpg"
+                  className="img-fluid rounded-start"
+                  variant="top"
+                  style={{ objectFit: "cover", height: "220px" }}
+                />
+              </Col>
+              <Col sm={4} md={6} lg={6}>
+                <Card.Body>
+                  <Card.Title as="h5">Sayeman Hotel & Resort</Card.Title>
+                  <Card.Text>
+                    <MdLocationOn /> &nbsp;14 Kalatoli Hotel Motel Zone, Cox's
+                    Bazar, Bangladesh
+                  </Card.Text>
+                  <Card.Text>*Rating Here</Card.Text>
+                  <Card.Text>*Trip Coin</Card.Text>
+                </Card.Body>
+              </Col>
+
+              <Col sm={4} md={3} lg={3} className="d-flex justify-content-end">
+                <Card.Body>
+                  <Card.Text className="my-3">Starts From</Card.Text>
+                  <Card.Text>BDT Magna/Night</Card.Text>
+
+                  <Button block className="mt-4">Book Now</Button>
+
+                  
+                  {/* <Link to="" className="btn btn-primary mb-5 d-flex flex-column">
+            Modify Search
+          </Link> */}
+                  
+                  
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
         </Col>
       </Row>
     </Container>
