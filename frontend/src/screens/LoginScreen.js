d8306fb4ff07872ed3a4c65a7c93fe37f73d0d7d
Sakib Patwary <shakibpatwary001@gmail.com>
2022-11-12 13:54:36 +0600

    reset password

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,98 +1,113 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import { Button, Form, Row, Col } from 'react-bootstrap'
-import { FcGoogle } from 'react-icons/fc'
-import FormContainer from '../components/FormContainer'
-import { signinLocal, resetAuth } from '../features/auth/authSlice'
-import { toast } from 'react-toastify'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import { FcGoogle } from "react-icons/fc";
+import FormContainer from "../components/FormContainer";
+import { signinLocal, resetAuth } from "../features/auth/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [showPassword, setShowPassword] = useState('password')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState("password");
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { userInfo, isError, isSuccess, isLoading, message } = useSelector(
     (state) => state.auth
-  )
+  );
 
   useEffect(() => {
     if (userInfo) {
-      navigate('/')
+      navigate("/");
     }
     if (isSuccess) {
-      toast.success('Logged in successfully', { position: 'top-center' })
-      navigate('/')
+      toast.success("Logged in successfully", { position: "top-center" });
+      navigate("/");
     }
     if (isError) {
-      toast.error(message, { position: 'top-center' })
+      toast.error(message, { position: "top-center" });
     }
 
-    dispatch(resetAuth())
-  }, [userInfo, isError, isSuccess, message, navigate, dispatch])
+    dispatch(resetAuth());
+  }, [userInfo, isError, isSuccess, message, navigate, dispatch]);
 
   const passwordShow = (e) => {
-    e.target.checked ? setShowPassword('text') : setShowPassword('password')
-  }
+    e.target.checked ? setShowPassword("text") : setShowPassword("password");
+  };
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(signinLocal({ email, password }))
-  }
+    e.preventDefault();
+    dispatch(signinLocal({ email, password }));
+  };
 
   const handleGoogle = () => {
-    console.log('hello')
-    window.open('http://localhost:5000/api/users/signin/google', '_self')
-  }
+    console.log("hello");
+    window.open("http://localhost:5000/api/users/signin/google", "_self");
+  };
 
   return (
     <FormContainer>
-      <h1 className='d-flex justify-content-center pt-1'>Sign In</h1>
+      <h1 className="d-flex justify-content-center pt-1">Sign In</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group className='mb-3' controlId='LoginEmail'>
+        <Form.Group className="mb-3" controlId="LoginEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
-            type='email'
-            placeholder='Enter email'
+            type="email"
+            placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
 
-        <Form.Group className='mb-3' controlId='LoginPassword'>
+        <Form.Group className="mb-3" controlId="LoginPassword">
           <Form.Label>Enter password</Form.Label>
           <Form.Control
             type={showPassword}
-            placeholder='Enter Password'
+            placeholder="Enter Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Form.Group className='mb-3' controlId='showPassword'>
-          <Form.Check
-            type='checkbox'
-            label='Show password'
-            onChange={(e) => passwordShow(e)}
-          />
-        </Form.Group>
-        <div className='d-grid gap-2'>
-          <Button variant='primary' size='lg' type='submit'>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="showPassword">
+              <Form.Check
+                type="checkbox"
+                label="Show password"
+                onChange={(e) => passwordShow(e)}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group
+              className="mb-3"
+              controlId="showPassword"
+            >
+              <Link to="/">
+              Forgot Password?
+            </Link>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="lg" type="submit">
             Login
           </Button>
         </div>
       </Form>
 
       <Row>
-        <h6 className='d-flex justify-content-center pt-5'>OR</h6>
-        <Col className='d-grid gap-2' sm={12} md={12} lg={12}>
+        <h6 className="d-flex justify-content-center pt-5">OR</h6>
+        <Col className="d-grid gap-2" sm={12} md={12} lg={12}>
           {/* <Link to='/'> */}
           <Button
-            variant='outline-danger'
-            align='end'
-            size='lg'
+            variant="outline-danger"
+            align="end"
+            size="lg"
             onClick={handleGoogle}
           >
             <FcGoogle /> Sign in with Google
@@ -101,27 +116,27 @@ const LoginScreen = () => {
       </Row>
 
       <Row>
-        <div className='d-flex justify-content-center py-3 mt-3 '>
+        <div className="d-flex justify-content-center py-3 mt-3 ">
           <p>
             New User?
-            <Link to='/registration'>
+            <Link to="/registration">
               <b>Sign Up</b>
             </Link>
           </p>
         </div>
       </Row>
 
-      <Row className='py-1 mt-2'>
+      <Row className="py-1 mt-2">
         <Col>
-          <Link to='/'>
-            <Button variant='primary' size='md'>
+          <Link to="/">
+            <Button variant="primary" size="md">
               Back to Home page
             </Button>
           </Link>
         </Col>
       </Row>
     </FormContainer>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
