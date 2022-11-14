0698a7f485b98c026cbd7b2068859727418360f6
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-11-14 16:36:09 +0600

    Business Owners Dashboard Updated

--- frontend/src/screens/BusinessOwnerDashoard.js
+++ frontend/src/screens/BusinessOwnerDashoard.js
@@ -11,14 +11,16 @@ import { RiSecurePaymentFill } from "react-icons/ri";
 
 const BusinessOwnerDashoard = () => {
   const [imageUrl, setImageUrl] = useState("");
-  const [serviceType, setServiceType] = useState('')
-  const [businessName, setBusinessName] = useState('')
-  const [licenseNumber, setLicenseNumber] = useState('')
-  const [uploadLicense, setUploadLicense] = useState('')
-  const [tinNumber, setTinNumber] = useState('')
-  const [location, setLocation] = useState('')
-  const [businessContact, setBusinessContact] = useState('')
-  
+  const [serviceType, setServiceType] = useState("");
+  const [businessName, setBusinessName] = useState("");
+  const [licenseNumber, setLicenseNumber] = useState("");
+  const [uploadLicense, setUploadLicense] = useState("");
+  const [tinNumber, setTinNumber] = useState("");
+  const [house, setHouse] = useState("");
+  const [street, setStreet] = useState("");
+  const [area, setArea] = useState("");
+  const [city, setCity] = useState("");
+  const [businessContact, setBusinessContact] = useState("");
 
   const uploadUserImageFileHandler = async (e) => {
     // const file = e.target.files[0]
@@ -174,102 +176,186 @@ const BusinessOwnerDashoard = () => {
                 Add a Business
               </Card.Header>
 
-              <Col lg={4} md={6} sm={12} className="d-grid gap-2 py-2">
-                <Card.Body>
-                  <Row>
-                    <Col
-                      className="d-flex justify-content-start"
-                      lg={6}
-                      md={6}
-                      sm={6}
-                    >
-                      <img
-                        className="mb-2"
-                        src={
-                          imageUrl !== ""
-                            ? imageUrl
-                            : "http://bootdey.com/img/Content/avatar/avatar7.png"
-                        }
-                        alt="User Image"
-                        style={{ height: "7rem", borderRadius: "50%" }}
-                      />
-                    </Col>
-                    <Col
-                      className="d-flex justify-content-end"
-                      lg={6}
-                      md={6}
-                      sm={6}
-                    >
-                      <Form.Group controlId="image 1">
-                        <Form.Label>
-                          <FaEdit className="me-2 mb-2" />
-                          Edit Picture
-                        </Form.Label>
-                        <Form.Control
-                          type="file"
-                          id="image-file"
-                          label="User Image"
-                          custom
-                          onChange={uploadUserImageFileHandler}
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Col>
-
-              <Col lg={8} md={6} sm={12}>
-                <Card>
+              <Row>
+                <Col lg={4} md={6} sm={12} className="d-grid gap-2 py-2">
                   <Card.Body>
-                    <h4>Business Information</h4>
                     <Row>
-                      <Col lg={12} md={12} sm={12}>
-                        <Form.Group className="mb-3" controlId="serviceType">
-                          <Form.Label>Service Type</Form.Label>
-                          <Form.Control
-                            as="select"
-                            type="select"
-                            placeholder="Select Service Type"
-                            value={serviceType}
-                            onChange={(e) => setServiceType(e.target.value)}
-                          >
-                            <option value='stays'>Accomodation</option>
-                            <option value='transport'>Transport</option>
-                            <option value='food'>Food</option>
-                            <option value='tours'>Tour Guide</option>
-                          </Form.Control>
-                        </Form.Group>
+                      <Col
+                        className="d-flex justify-content-start"
+                        lg={6}
+                        md={6}
+                        sm={6}
+                      >
+                        <img
+                          className="mb-2"
+                          src={
+                            imageUrl !== ""
+                              ? imageUrl
+                              : "http://bootdey.com/img/Content/avatar/avatar7.png"
+                          }
+                          alt="User Image"
+                          style={{ height: "7rem", borderRadius: "50%" }}
+                        />
                       </Col>
-
-                      <Col lg={6} md={12} sm={12}>
-                        <Form.Group className="mb-3" controlId="businessName">
-                          <Form.Label>Business Institute Name</Form.Label>
+                      <Col
+                        className="d-flex justify-content-end"
+                        lg={6}
+                        md={6}
+                        sm={6}
+                      >
+                        <Form.Group controlId="image 1">
+                          <Form.Label>
+                            <FaEdit className="me-2 mb-2" />
+                            Edit Picture
+                          </Form.Label>
                           <Form.Control
-                            type="text"
-                            placeholder="Enter Business Institute Name"
-                            value={businessName}
-                            onChange={(e) => setBusinessName(e.target.value)}
-                          >
-                          </Form.Control>
-                        </Form.Group>
-                      </Col>
-
-                      <Col lg={6} md={12} sm={12}>
-                        <Form.Group className="mb-3" controlId="licenseNumber">
-                          <Form.Label>License Number</Form.Label>
-                          <Form.Control
-                            type="text"
-                            placeholder="Enter License Number"
-                            value={licenseNumber}
-                            onChange={(e) => setLicenseNumber(e.target.value)}
-                          >
-                          </Form.Control>
+                            type="file"
+                            id="image-file"
+                            label="User Image"
+                            custom
+                            onChange={uploadUserImageFileHandler}
+                          ></Form.Control>
                         </Form.Group>
                       </Col>
                     </Row>
                   </Card.Body>
-                </Card>
-              </Col>
+                </Col>
+
+                <Col lg={8} md={6} sm={12}>
+                  <Card>
+                    <Card.Body>
+                      <h4>Business Information</h4>
+                      <Row>
+                        <Col lg={12} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="serviceType">
+                            <Form.Label>Service Type</Form.Label>
+                            <Form.Control
+                              as="select"
+                              type="select"
+                              placeholder="Select Service Type"
+                              value={serviceType}
+                              onChange={(e) => setServiceType(e.target.value)}
+                            >
+                              <option value="stays">Accomodation</option>
+                              <option value="transport">Transport</option>
+                              <option value="food">Food</option>
+                              <option value="tours">Tour Guide</option>
+                            </Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="businessName">
+                            <Form.Label>Business Institute Name</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter Business Institute Name"
+                              value={businessName}
+                              onChange={(e) => setBusinessName(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="licenseNumber"
+                          >
+                            <Form.Label>License Number</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter License Number"
+                              value={licenseNumber}
+                              onChange={(e) => setLicenseNumber(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="tinNumber">
+                            <Form.Label>TIN Number</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter TIN Number"
+                              value={tinNumber}
+                              onChange={(e) => setTinNumber(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                      <h4>Business Address Information</h4>
+                      <Row>
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="house">
+                            <Form.Label>House Number</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter House Number"
+                              value={house}
+                              onChange={(e) => setHouse(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="street">
+                            <Form.Label>Street Number</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter Street"
+                              value={street}
+                              onChange={(e) => setStreet(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="area">
+                            <Form.Label>Area</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Area"
+                              value={area}
+                              onChange={(e) => setArea(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+
+                        <Col lg={6} md={12} sm={12}>
+                          <Form.Group className="mb-3" controlId="city">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="City Name"
+                              value={city}
+                              onChange={(e) => setCity(e.target.value)}
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                      <h4>Business Contact Information</h4>
+                      <Row>
+                        <Col lg={12} md={12} sm={12}>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="businessContact"
+                          >
+                            <Form.Label>Contact No</Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter Verified Business Contact No."
+                              value={businessContact}
+                              onChange={(e) =>
+                                setBusinessContact(e.target.value)
+                              }
+                            ></Form.Control>
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
             </Card>
           </Form>
         </Col>
