c9c866cdce94c7acf5192a2b1bdf9c65a347d435
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-12-05 13:34:55 +0600

    stays search screen data fetch updated

--- frontend/src/screens/StaysSearchScreen.js
+++ frontend/src/screens/StaysSearchScreen.js
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useSearchParams } from 'react-router-dom'
-import { Row, Col, Container, Card, Form, Button } from 'react-bootstrap'
-import { MdLocationOn } from 'react-icons/md'
-import { useDispatch, useSelector } from 'react-redux'
-import { LinkContainer } from 'react-router-bootstrap'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import { toast } from 'react-toastify'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { Row, Col, Container, Card, Form, Button } from "react-bootstrap";
+import { MdLocationOn } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { toast } from "react-toastify";
 import {
   getAllAcomodations,
   resetServiceList,
-} from '../features/service/serviceSlice'
-import SearchStays from '../components/SearchStays'
+} from "../features/service/serviceSlice";
+import SearchStays from "../components/SearchStays";
 
 const StaysSearchScreen = () => {
-  const [maxPrice, setMaxPrice] = useState('')
-  const [minPrice, setMinPrice] = useState('')
-  const [searchHotel, setSearchHotel] = useState('')
+  const [maxPrice, setMaxPrice] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [searchHotel, setSearchHotel] = useState("");
 
-  const dispatch = useDispatch()
-  const [searchParams] = useSearchParams()
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
 
-  const [allAccomodations, setAllAccomodations] = useState([])
+  const [allAccomodations, setAllAccomodations] = useState([]);
   const [checkinDateSearch, setCheckinDateSearch] = useState(
-    searchParams.get('checkinDate') || null
-  )
+    searchParams.get("checkinDate") || null
+  );
   const [checkoutDateSearch, setCheckoutDateSearch] = useState(
-    searchParams.get('checkoutDate') || null
-  )
+    searchParams.get("checkoutDate") || null
+  );
   const [guestCountSearch, setGuestCountSearch] = useState(
-    searchParams.get('guestCount') || ''
-  )
+    searchParams.get("guestCount") || 1
+  );
   const [roomCountSearch, setRoomCountSearch] = useState(
-    searchParams.get('roomCount') || ''
-  )
-  const [modifySearch, setModifySearch] = useState(false)
+    searchParams.get("roomCount") || 1
+  );
+  const [modifySearch, setModifySearch] = useState(false);
 
   const {
     accomodations,
@@ -42,105 +42,123 @@ const StaysSearchScreen = () => {
     isListSuccess,
     isListError,
     listErrorMessage,
-  } = useSelector((state) => state.service)
+  } = useSelector((state) => state.service);
 
   useEffect(() => {
     if (isListError) {
-      toast.error(listErrorMessage, { position: 'top-center' })
+      toast.error(listErrorMessage, { position: "top-center" });
     } else if (isListSuccess) {
       const filteredServices = accomodations
         .filter((service) => {
-          if (checkinDateSearch === '') {
-            return service
-          } else if (service.accomodationInfo.checkinDate === checkinDateSearch) {
-            return service
+          if (checkinDateSearch === null) {
+            return service;
+          } else if (
+            new Date(service.accomodationInfo.checkinDate)
+              .toISOString()
+              .split("T")[0] === checkinDateSearch
+          ) {
+            return service;
           }
         })
         .filter((service) => {
-          if (checkoutDateSearch === '') {
-            return service
-          } else if (service.accomodationInfo.checkoutDate === checkoutDateSearch) {
-            return service
+          if (checkoutDateSearch === null) {
+            return service;
+          } else if (
+              new Date(service.accomodationInfo.checkoutDate)
+              .toISOString()
+              .split("T")[0] === checkoutDateSearch
+          ) {
+            return service;
           }
         })
         .filter((service) => {
           if (guestCountSearch === 0) {
-            return service
-          } else if (service.accomodationInfo.maxGuests === guestCountSearch) {
-            return service
+            return service;
+          } else if (service.accomodationInfo.maxGuests >= guestCountSearch) {
+            return service;
           }
         })
         .filter((service) => {
           if (roomCountSearch === 0) {
-            return service
-          } else if (service.accomodationInfo.rooms === roomCountSearch) {
-            return service
+            return service;
+          } else if (service.accomodationInfo.rooms >= roomCountSearch) {
+            return service;
           }
-        })
-      setAllAccomodations(filteredServices)
+        });
+      setAllAccomodations(filteredServices);
     } else {
-      dispatch(getAllAcomodations())
+      dispatch(getAllAcomodations());
     }
-  }, [dispatch, checkinDateSearch, checkoutDateSearch, guestCountSearch, roomCountSearch, isListError, isListSuccess, accomodations, listErrorMessage])
+  }, [
+    dispatch,
+    checkinDateSearch,
+    checkoutDateSearch,
+    guestCountSearch,
+    roomCountSearch,
+    isListError,
+    isListSuccess,
+    accomodations,
+    listErrorMessage,
+  ]);
 
   useEffect(() => {
     return () => {
-      dispatch(resetServiceList())
-    }
-  }, [dispatch])
+      dispatch(resetServiceList());
+    };
+  }, [dispatch]);
 
   return (
     <Container>
-      <Row className='mb-2 pt-3'>
+      <Row className="mb-2 pt-3">
         <Col lg={8} md={8} sm={6}>
-          <Card.Text as='h3'>Location Name</Card.Text>
+          <Card.Text as="h3">Location Name</Card.Text>
           <Card.Text>
             {!checkinDateSearch &&
             !checkoutDateSearch &&
             !guestCountSearch &&
             !roomCountSearch
-              ? 'Find Your Desired Accomodations or Hotels'
+              ? "Find Your Desired Accomodations or Hotels"
               : `Search Queries (Check In Date : ${checkinDateSearch}, Check Out Date : ${checkoutDateSearch}, Guest(s) : ${guestCountSearch}, Room(s) : ${roomCountSearch})`}
           </Card.Text>
         </Col>
-        <Col lg={3} md={3} sm={6} className='d-flex justify-content-end'>
+        <Col lg={3} md={3} sm={6} className="d-flex justify-content-end">
           <Button onClick={() => setModifySearch(!modifySearch)}>
-            {modifySearch ? 'Cancel Search' : 'Modify Search'}
+            {modifySearch ? "Cancel Search" : "Modify Search"}
           </Button>
         </Col>
       </Row>
 
-      <Row className='my-3'>{modifySearch && <SearchStays />}</Row>
+      <Row className="my-3">{modifySearch && <SearchStays />}</Row>
 
       <Row>
         {/* Left Column */}
         <Col xs={12} md={3} xl={3}>
-          <Row className='my-3'>
-            <Link to='' className='btn btn-outline-primary'>
+          <Row className="my-3">
+            <Link to="" className="btn btn-outline-primary">
               Reset Search
             </Link>
           </Row>
 
-          <Row className='my-4'>
+          <Row className="my-4">
             <Card>
-              <Card.Header as='h5'>Price Range</Card.Header>
+              <Card.Header as="h5">Price Range</Card.Header>
             </Card>
 
             <Col sm={6} md={3} lg={6}>
-              <Form.Group className='mb-3' controlId=''>
-                <Form.Label className='small mb-1'>Minimum Price</Form.Label>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label className="small mb-1">Minimum Price</Form.Label>
                 <Form.Control
-                  type='text'
+                  type="text"
                   value={minPrice}
                   onChange={(e) => setMinPrice(e.target.value)}
                 ></Form.Control>
               </Form.Group>
             </Col>
             <Col sm={6} md={3} lg={6}>
-              <Form.Group className='mb-3' controlId=''>
-                <Form.Label className='small mb-1'>Maximum Price</Form.Label>
+              <Form.Group className="mb-3" controlId="">
+                <Form.Label className="small mb-1">Maximum Price</Form.Label>
                 <Form.Control
-                  type='text'
+                  type="text"
                   value={maxPrice}
                   onChange={(e) => setMaxPrice(e.target.value)}
                 ></Form.Control>
@@ -170,11 +188,11 @@ const StaysSearchScreen = () => {
 
         {/* Right Colomn/Package Images Card */}
         <Col xs={12} md={9} xl={9}>
-          <Row className='my-4'>
-            <Card.Header as='h5' className='mx-4'>
+          <Row className="my-4">
+            <Card.Header as="h5" className="mx-4">
               Available Hotels
             </Card.Header>
-            <Card.Text className='mx-3 mt-2'>
+            <Card.Text className="mx-3 mt-2">
               *Price is per night per room & includes VAT & Taxes
             </Card.Text>
           </Row>
@@ -182,28 +200,34 @@ const StaysSearchScreen = () => {
           {isListLoading ? (
             <Loader />
           ) : allAccomodations.length <= 0 ? (
-            <Message variant='danger'>No Hotels or Accomodations Found. Please Modify Search Queries!</Message>
+            <Message variant="danger">
+              No Hotels or Accomodations Found. Please Modify Search Queries!
+            </Message>
           ) : (
             <Card>
               {allAccomodations.map((accomodation) => (
-                <LinkContainer to=''>
-                  <Card className='mx-2 my-3' key={accomodation._id}>
-                    <Row className='d-flex'>
+                <LinkContainer to="">
+                  <Card className="mx-2 my-3" key={accomodation._id}>
+                    <Row className="d-flex">
                       <Col sm={4} md={3} lg={3}>
                         <Card.Img
-                          src='/Destinations/Test.jpg'
-                          className='img-fluid rounded-start'
-                          variant='top'
-                          style={{ objectFit: 'cover', height: '220px' }}
+                          src="/Destinations/Test.jpg"
+                          className="img-fluid rounded-start"
+                          variant="top"
+                          style={{ objectFit: "cover", height: "220px" }}
                         />
                       </Col>
                       <Col sm={4} md={6} lg={6}>
                         <Card.Body>
-                          <Card.Title as='h5'>{accomodation.accomodationInfo.address.house}</Card.Title>
+                          <Card.Title as="h5">
+                            {accomodation.accomodationInfo.address.house}
+                          </Card.Title>
                           <Card.Text>
-                            <MdLocationOn /> &nbsp;{accomodation.accomodationInfo.address.street}
+                            <MdLocationOn /> &nbsp;
+                            {accomodation.accomodationInfo.address.street}
                             {accomodation.accomodationInfo.address.area}
-                            {accomodation.accomodationInfo.address.city}, Bangladesh
+                            {accomodation.accomodationInfo.address.city},
+                            Bangladesh
                           </Card.Text>
                           <Card.Text>*Rating Here</Card.Text>
                           <Card.Text>*Trip Coin</Card.Text>
@@ -214,13 +238,13 @@ const StaysSearchScreen = () => {
                         sm={4}
                         md={3}
                         lg={3}
-                        className='d-flex justify-content-end'
+                        className="d-flex justify-content-end"
                       >
                         <Card.Body>
-                          <Card.Text className='my-3'>Price</Card.Text>
+                          <Card.Text className="my-3">Price</Card.Text>
                           <Card.Text>BDT {accomodation.price}</Card.Text>
 
-                          <Button className='mt-4'>Book Now</Button>
+                          <Button className="mt-4">Book Now</Button>
                         </Card.Body>
                       </Col>
                     </Row>
@@ -232,7 +256,7 @@ const StaysSearchScreen = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default StaysSearchScreen
+export default StaysSearchScreen;
