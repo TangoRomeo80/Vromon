853723e8d08b65986bf0d15af3b62280f58f06b1
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-12-18 17:43:53 +0600

    transportDetailsBusiness Updated

--- frontend/src/screens/TransportDetailBusiness.js
+++ frontend/src/screens/TransportDetailBusiness.js
@@ -1,9 +1,42 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Card, Row, Col, Form, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import {
+  getTransportById,
+  updateService,
+  deleteService,
+  resetServiceDetails,
+  resetServiceUpdate,
+  resetServiceDelete,
+} from "../features/service/serviceSlice";
 
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const TransportDetailBusiness = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
 
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const {
+    transport,
+    isDetailsLoading,
+    isDetailsError,
+    isDetailsSuccess,
+    detailsErrorMessage,
+    isUpdateLoading,
+    isUpdateError,
+    isUpdateSuccess,
+    updateErrorMessage,
+    isDeleteLoading,
+    isDeleteError,
+    isDeleteSuccess,
+    deleteErrorMessage,
+  } = useSelector((state) => state.service);
 
   const [transportType, setTransportType] = useState("");
   const [departFrom, setDepartFrom] = useState("");
@@ -20,7 +53,7 @@ const TransportDetailBusiness = () => {
   const [pickUpDate, setPickUpDate] = useState("");
   const [dropOffDate, setDropOffDate] = useState("");
   const [pickUpTime, setPickUpTime] = useState("");
-  const [dropOffTime, setDropOffTime]= useState("");
+  const [dropOffTime, setDropOffTime] = useState("");
   const [driverName, setDriverName] = useState("");
   const [driverContact, setDriverContact] = useState("");
   const [driverLicense, setDriverLicense] = useState("");
@@ -28,6 +61,55 @@ const TransportDetailBusiness = () => {
   const [carRegistrationImage, setCarRegistrationImage] = useState("");
   const [carModel, setCarModel] = useState("");
   const [carType, setCarType] = useState("");
+
+  useEffect(() => {
+    if(isDetailsError){
+      toast.error(detailsErrorMessage, { position: 'top-center' });
+    }
+    else if(isDetailsSuccess){
+      setTransportType(transport.transportInfo.transportType);
+      setDepartFrom(transport.transportInfo.departFrom);
+      setDepartTo(transport.transportInfo.departTo);
+      setDepartDate(transport.transportInfo.departDate);
+      setDepartTime(transport.transportInfo.departTime);
+      setArrivalTime(transport.transportInfo.arrivalTime);
+      setReturnDate(transport.transportInfo.returnDate);
+      setReturnTime(transport.transportInfo.returnTime);
+      setPickupFrom(transport.transportInfo.pickupFrom);
+      setDropTo(transport.transportInfo.dropTo);
+      setRentDuration(transport.transportInfo.rentDuration);
+      setPickUpDate(transport.transportInfo.pickUpDate);
+      setDropOffDate(transport.transportInfo.dropOffDate);
+      setPickUpTime(transport.transportInfo.pickUpTime);
+      setDropOffTime(transport.transportInfo.dropOffTime);
+      setDriverName(transport.transportInfo.driverName);
+      setDriverContact(transport.transportInfo.driverContact);
+      setDriverLicense(transport.transportInfo.driverLicense);
+      setCarRegistration(transport.transportInfo.carRegistration);
+      setCarRegistrationImage(transport.transportInfo.carRegistrationImage);
+      setCarModel(transport.transportInfo.carModel);
+      setCarType(transport.transportInfo.carType);
+    }
+    else {
+      dispatch(getTransportById(params.id));
+    }
+  }, [dispatch, transport, isDetailsSuccess, isDetailsError, detailsErrorMessage])
+
+  useEffect(() => {
+    if(isUpdateError){
+      toast.error(updateErrorMessage, { position: 'top-center' });
+    }
+    else if(isUpdateSuccess){
+      toast.success('Transport Details Updated Successfully', { position: 'top-center' });
+    }
+  })
+
+  useEffect(() => {
+    return() => {
+      dispatch(resetServiceDetails())
+      dispatch(resetServiceUpdate())
+    }
+  }, [dispatch])
 
 
   return (
@@ -100,9 +182,7 @@ const TransportDetailBusiness = () => {
                   </Col>
                   <Col lg={6} md={6} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Car Type
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Car Type</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
@@ -111,9 +191,7 @@ const TransportDetailBusiness = () => {
                 <Row>
                   <Col lg={12} md={12} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Car Model
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Car Model</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
@@ -122,17 +200,13 @@ const TransportDetailBusiness = () => {
                 <Row>
                   <Col lg={6} md={6} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Pick From
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Pick From</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
                   <Col lg={6} md={6} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Drop To
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Drop To</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
@@ -141,17 +215,13 @@ const TransportDetailBusiness = () => {
                 <Row>
                   <Col lg={6} md={6} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Pick Time
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Pick Time</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
                   <Col lg={6} md={6} sm={12}>
                     <Form.Group className="mb-3" controlId="transportName">
-                      <Form.Label className="small mb-1">
-                        Drop Ttime
-                      </Form.Label>
+                      <Form.Label className="small mb-1">Drop Ttime</Form.Label>
                       <Form.Control required type="text"></Form.Control>
                     </Form.Group>
                   </Col>
