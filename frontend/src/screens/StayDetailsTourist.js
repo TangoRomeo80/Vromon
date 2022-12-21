253c314813a53218659d6da4e498aed476f72dd8
diponkorcs <66906170+diponkorcs@users.noreply.github.com>
2022-12-21 15:58:46 +0600

    StayDetailsTourist UI Updated

--- frontend/src/screens/StayDetailsTourist.js
+++ frontend/src/screens/StayDetailsTourist.js
@@ -1,61 +1,94 @@
 import React from "react";
-import { Container, Card, Row, Col, Form, Button } from "react-bootstrap";
-
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Form,
+  Button,
+  Carousel,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 const StayDetailsTourist = () => {
-
-  
   return (
-    <Container className="pt-5">
-    <Row className="pb-5">
-      <Card.Text as="h2" className="font-weight-bolder text-center">
-        Stays Details
-      </Card.Text>
-    </Row>
+    <Container className="pt-4">
+      <Row className="pb-4">
+        <Card.Text as="h2" className="font-weight-bolder text-center">
+          Details Information of (Name)
+        </Card.Text>
+      </Row>
 
-    <Form>
       <Row>
-        <Col xs={12} md={4} xl={3}>
-          <Card className="mb-4">
-            <Card.Header>Stays Image</Card.Header>
-            <Card.Body className="text-center">
-                <Card.Img
-                src='/destinations/test.png'
-                />
-            </Card.Body>
+        <Col>
+          <Card>
+            <Card.Img
+              cascade
+              className="img-fluid"
+              src="/uploads/stays-2.jpg"
+              style={{ maxHeight: "45vh" }}
+            />
           </Card>
         </Col>
+      </Row>
 
-        <Col xs={12} md={8} xl={9}>
-          <Card className="mb-4">
-            <Card.Header>Stays Information</Card.Header>
+      <h3 className="my-4 d-flex justify-content-center">
+        Detailed Information
+      </h3>
+
+      <Row>
+        <Col lg={6} md={6} sm={12}>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="/destinations/test.jpg"
+                alt="Destination Images"
+                style={{ maxHeight: "45vh", objectFit: "cover" }}
+              />
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+        <Col lg={6} md={6} sm={12}>
+          <Card>
+            <Card.Header as="h3" className="text-center">
+              Information About (Name)
+            </Card.Header>
             <Card.Body>
-              <Row>
-                <Col lg={6} md={6} sm={12}>
-                  <Form.Group className="mb-3" controlId="transportName">
-                    <Form.Label className="small mb-1">
-                      Stays Name
-                    </Form.Label>
-                    <Form.Control required type="text"></Form.Control>
-                  </Form.Group>
-                </Col>
-                <Col lg={6} md={6} sm={12}>
-                  <Form.Group className="mb-3" controlId="transportName">
-                    <Form.Label className="small mb-1">
-                      Stays Location
-                    </Form.Label>
-                    <Form.Control required type="text"></Form.Control>
-                  </Form.Group>
-                </Col>
-              </Row>
+              <Card.Text>
+                <strong>Hotel Name</strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Location</strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Rooms Available : </strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Max Guests (Per Room) : </strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Cost (Per Room) : </strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Discount : %</strong>
+              </Card.Text>
+              <Card.Text>
+                <strong>Rating : </strong>
+              </Card.Text>
+              <Link to="" className="btn btn-primary">
+                  Write a Review
+                </Link>
+                <Link to="" className="btn btn-primary">
+                  Check All Reviews
+                </Link>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-      
-    </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default StayDetailsTourist
+export default StayDetailsTourist;
