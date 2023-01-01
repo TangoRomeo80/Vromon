7b387ba2428fdba801feac6fd19bcf69e55357de
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-01-01 19:03:34 +0600

    create error screen

--- frontend/src/screens/ErrorScreen.js
+++ frontend/src/screens/ErrorScreen.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const ErrorScreen = () => {
-  return <div style={{ backgroundColor: 'red' }}>ErrorScreen</div>
+  return (
+    <Container>
+      <Row>
+        <Col className='d-flex justify-content-center align-items-center'>
+          <h1>Not Found</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center align-items-center'>
+          <p>Sorry, the page you are looking for could not be found.</p>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ErrorScreen
